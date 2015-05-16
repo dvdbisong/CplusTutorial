32ec458cc4f3499d3989d5595e3d09b60d0d4963
dvdbisong <dvdbisong@gmail.com>
2015-05-17 00:14:37 +0100

    update - assignment  stubs

---
+++ 2. programming assignments/Assignment #1/Question5stub.cpp
@@ -0,0 +1,31 @@
+/*
+ *  Question5.cpp
+ *
+ *  Name:           [ enter your name here ]
+ *  Department:     [ enter your department here ]
+ *  Matric. No:     [ enter matric no. here ]
+ *
+ *  Program that calculates the squares and cubes of the integers from 0 to 10
+ *
+ *  Stub file to enable you complete assignment #1 - question #5
+ */
+
+#include <iostream>
+#include <math.h>   // library to use pow() function to calculate squares and cubes
+using namespace std;
+
+int main()
+{
+    
+    // display welcome messages to user
+    cout << "Welcome - This program calculates the squares and cubes\n";
+    cout << "of the integers from 0 to 10 and prints using tabs\n\n";
+    
+    // TODO: Hint use pow(base, power) to calculate squares and cubes
+    // Put code here ...
+    
+    cout << "\nThanks and Goodbye";
+    
+    //system("PAUSE");
+    return EXIT_SUCCESS;
+}
